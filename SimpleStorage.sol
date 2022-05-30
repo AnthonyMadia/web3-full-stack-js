9a4c96dc3c9592195a3eed6e5b131a93bbc3a504
Anthony Madia <aamadia24@gmail.com>
2022-05-29 21:53:19 -0400

    Add struct notes and code

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,12 +5,53 @@ pragma solidity ^0.8.7; // ^ and >= < operators for version control
 // Every single smart contract has an address
 contract SimpleStorage {
     // default value is 0
+    // default visibility is internal
+    // favoriteNumber is in global scope
+
+    // set to public, these variables contain a GETTER function 
     uint public favoriteNumber;
+    People public person = People({favoriteNumber: 2, name: "Dory"});
+
+    // now, we can use this People type
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber; // semi here not after }
+       
     }
+
+    // store is ORANGE in remix
+        // ORANGE are functions that do not have to spend gas to run
+        // VIEW and PURE
+    // View means a function that we are only going to read State off of contract
+        // For example, retrieve function is just going to read what our favoriteNumber is
+        // remember, Gas is only spent when we change state of the blockchain
+    // View and pure functions do not allow modifications of state
+    
+
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+    // Pure functions additionally disallow you to read from blockchain state
+    function add() public pure returns (uint256) {
+        return (1 + 1);
+    }
+    // view or pure functions only costs gas when a gas function calls it
 }
+
+// Basic Solidity Arrays and Structs
+    // Struct example
+        // store different people who have different favoriteNumbers
+
+
+
+
+
+
+
 
 // Contract address: 0xd9145CCE52D386f254917e481eB44e9943F39138
 
@@ -24,3 +65,5 @@ contract SimpleStorage {
     // private - only visible in the current contract
     // external - only visible externally - i.e. can only be message-called
     // internal - only visible internally
+
+// The more computation the function does, the more gas it costs
