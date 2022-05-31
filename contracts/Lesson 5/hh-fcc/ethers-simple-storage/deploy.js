4b40125634094fffec7fbb165341c11318671466
Anthony Madia <aamadia24@gmail.com>
2022-05-30 20:16:22 -0400

    Get abi, binary, and call functions with contractFactory

--- contracts/Lesson 5/hh-fcc/ethers-simple-storage/deploy.js
+++ contracts/Lesson 5/hh-fcc/ethers-simple-storage/deploy.js
@@ -8,21 +8,35 @@
 const ethers = require("ethers")
 // to read from abi and bin files 
 const fs = require("fs-extra")
+require("dotenv").config()
 
 
 async function main() {
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545")
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
     const wallet = new ethers.Wallet(
-        "5b538472f48fb953f5b2bea479a4916802dbe01bb6a3a93442291510cde7eed8",
+        process.env.PRIVATE_KEY,
         provider
     )
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8")
+
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
     console.log("Deploying, please wait...")
     // deploy with ethers
-    const contract = await contractFactory.deploy() // STOP HERE! Wait for contract to deploy
-    console.log(contract)
+    const contract = await contractFactory.deploy({
+        gasPrice: 1000000000
+    }) // STOP HERE! Wait for contract to deploy
+    await contract.deployTransaction.wait(1)
+    
+
+    // retrieve is a view function so will not cost any gas
+    const currentFavoriteNumber = await contract.retrieve()
+    console.log(currentFavoriteNumber.toString()) // stringify to make more readable
+
+    const transactionResponse = await contract.store("7")
+    const transactionReceipt = await transactionResponse.wait(1)
+    const updatedfavoriteNumber = await contract.retrieve()
+    console.log(`🚀 ~ updatedfavoriteNumber, ${updatedfavoriteNumber}`);
 }
 
 main()
