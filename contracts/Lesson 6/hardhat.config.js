481ffca0f68c65d1736e3be50fb1d450ec4b1c32
Anthony Madia <aamadia24@gmail.com>
2022-06-13 15:21:01 -0400

    Add Rinkeby RPC from Alchemy

--- contracts/Lesson 6/hardhat.config.js
+++ contracts/Lesson 6/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config()
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -16,6 +16,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL
+    },
+  },
   solidity: "0.8.8",
 };
