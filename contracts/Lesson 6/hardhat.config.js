38acb3b02e23eea5abd40e6bb2205ce7522e836f
Anthony Madia <aamadia24@gmail.com>
2022-06-13 15:36:11 -0400

    Deploy SimpleStorage to Rinkeby

--- contracts/Lesson 6/hardhat.config.js
+++ contracts/Lesson 6/hardhat.config.js
@@ -18,12 +18,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     rinkeby: {
-      url: RINKEBY_RPC_URL
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4
     },
   },
   solidity: "0.8.8",
