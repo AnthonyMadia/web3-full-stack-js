9f29108722394bbd2158bef578d9184bc89fbb63
Anthony Madia <aamadia24@gmail.com>
2022-08-24 11:48:32 -0400

    Add: lesson8

--- Lessons/Lesson8/index.js
+++ Lessons/Lesson8/index.js
@@ -4,6 +4,7 @@
 // in front-end js you can't use require
 // use import
 import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
 
 // Cached element references
 const connectButton = document.getElementById("connectButton")
@@ -29,14 +30,37 @@ async function connect() {
 }
 
 // fund function
-async function fund(ethAmount) {
+async function fund() {
+  const ethAmount = "77"
   console.log(`Funding with ${ethAmount}`)
   if (typeof window.ethereum !== "undefined") {
     // need provider / connection to the blockchain
     // signer / wallet / someone with some gas
     // contract we are interacting with
     // need ^ ABI and Address
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      })
+      // hey, wait for this tx to be mined
+      await listenForTransactionMine(transactionResponse, provider)
+      console.log("Done!")
+    } catch (error) {
+      console.log(error)
+    }
   }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}...`)
+  provider.once(transactionResponse.hash, (transactionReceipt) => {
+    console.log(
+      `Completed with ${transactionReceipt.confirmations} confirmations`
+    )
+  })
 }
 
 // withdraw
