c7724f5409ba75f645eb897a1bad66958a404d91
Anthony Madia <aamadia24@gmail.com>
2022-08-24 13:46:23 -0400

    Set up Lesson9

---
+++ Lessons/Lesson9/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -0,0 +1,9 @@
+// Raffle 
+
+// Enter the lottery (paying some amount)
+// Pick a random winnder (verifiably random)
+// Winner selected every X minutes -> completely automated
+
+// Chainlink oracle -> Randomness, Automated Execution (Chainlink Keepers)
+
+// SPDX-License-Identifer: MIT
